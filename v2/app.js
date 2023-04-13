71d612ef64a8f27d752cae01cde40733ee7da708
learnbyseven <34051943+learnbyseven@users.noreply.github.com>
2023-04-13 15:42:44 +1000

    Update app.js

--- v2/app.js
+++ v2/app.js
@@ -2,24 +2,24 @@
 import chalk from 'chalk';
 import express from 'express';
 import os from 'os';
-const v1 = express();
+const v2 = express();
 const server_Hostname = os.hostname();
 
 //LISTENERS
-v1.listen(80);
+v2.listen(80);
 
 //EXPRESS 
-v1.use((req, res, next) => {
+v2.use((req, res, next) => {
     console.log('Req executed from Source IP', req.ip);
     next();
 })
 
 //FOR V1 ON PORT 80
-v1.get('/',(req, res, next) => {
+v2.get('/',(req, res, next) => {
    res.send(chalk.green(' Informatica Version-2.0\n','Deployment: GREEN\n','HOST:',server_Hostname,'\n'))
 });
-v1.set ('json spaces', 2);
-v1.get('/healthz', (req, res) => {
+v2.set ('json spaces', 2);
+v2.get('/healthz', (req, res) => {
     res.json({'health': 'OK',})
 });
 //END
